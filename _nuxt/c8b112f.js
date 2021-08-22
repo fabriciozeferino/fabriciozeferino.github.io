f2b2867746069dd12b6429f3842df2881b2f8f08
fabriciozeferino <fabriciozeferino@users.noreply.github.com>
2021-08-22 17:27:38 +0000

    deploy: 1e6b6cb607b98de7b18aa839805a9979cba8a517

--- _nuxt/c8b112f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{214:function(e,n,t){"use strict";t.r(n);var l={name:"BaseAvatar"},r=t(35),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
