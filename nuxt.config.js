88b26934639759926c4566282fa76279a5dd2d10
Fabricio Zeferino <fabriciozeferino84@gmail.com>
2021-08-22 18:20:00 +0100

    ci

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
-  router: {
-    base: "fabriciozeferino.github.io",
-  },
+  // router: {
+  //   base: "fabriciozeferino.github.io",
+  // },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
