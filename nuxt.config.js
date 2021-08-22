ee3873d64b471259d95117e0b34d161cd19c5b8a
Fabricio Zeferino <fabriciozeferino84@gmail.com>
2021-08-22 18:38:57 +0100

    add SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,12 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Fabricio Zeferino full-stack web developer based in England. Laravel, vue.js, docker, relational database.",
+      },
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
