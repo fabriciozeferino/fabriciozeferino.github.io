1e6b6cb607b98de7b18aa839805a9979cba8a517
Fabricio Zeferino <fabriciozeferino84@gmail.com>
2021-08-22 18:26:20 +0100

    fix avatar

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -1,6 +1,17 @@
 <template>
-  <img class=" mr-1 w-28 h-28 object-cover rounded-full shadow-md inline-block
-  border-4" src=https://avatars.githubusercontent.com/u/37294999" alt="avatar"
+  <img
+    class="
+      mr-1
+      w-28
+      h-28
+      object-cover
+      rounded-full
+      shadow-md
+      inline-block
+      border-4
+    "
+    src="https://avatars.githubusercontent.com/u/37294999"
+    alt="avatar"
   />
 </template>
 
