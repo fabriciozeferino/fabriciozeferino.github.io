f5aa312082c8a3c3160bc6d3d653ad246710ec56
fabriciozeferino <fabriciozeferino@users.noreply.github.com>
2021-08-22 15:57:50 +0000

    deploy: 5da2cdc011e39017589a5c8bfe8471d0cd8b7776

---
+++ _nuxt/static/1629647869/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"fabriciozeferino.github.io\u002F_nuxt\u002Fstatic\u002F1629647869",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"fabriciozeferino.github.io\u002F",assetsPath:"fabriciozeferino.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
