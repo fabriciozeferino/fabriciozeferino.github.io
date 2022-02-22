7337fc9a1d0124962a10a066827a00e2e8b82ccd
Fabricio Zeferino <37294999+fabriciozeferino@users.noreply.github.com>
2022-02-22 08:25:34 +0000

    fetch CV data from json file

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,48 @@
 <template>
   <div class="antialiased bg-gray-50 md:py-12 text-xs">
     <div class="page sm:flex print:flex mx-auto">
-      <div class="sm:w-4/12 pt-6 px-4 bg-green-900 text-white flex flex-col">
+      <div class="sm:w-4/12 pt-6 px-4 bg-cyan-900 text-white flex flex-col">
         <Avatar class="self-center mb-2" />
 
-        <h1 class="text-center font-bold text-2xl">Fabricio Zeferino</h1>
-        <p class="text-center mb-4">Full Stack Developer</p>
+        <h1 class="text-center font-bold text-2xl">
+          {{ content.name }}
+        </h1>
+        <p class="text-center mb-4">
+          {{ content.job_title }}
+        </p>
 
         <div class="mb-4">
-          <Topic icon="profile" text="Profile" />
+          <Topic
+            icon="profile"
+            text="Profile"
+          />
           <p>
-            &#xA0; A passionate full-stack self-taught developer that is aways
-            aiming to learn. I’ve commercial experience working in an
-            application built in Laravel, jQuery, Bootstrap and MySQL where I
-            was responsible for making new features from scratch, going through
-            all the areas of the development.
+            {{ content.profile_description }}
           </p>
         </div>
 
         <div class="mb-4">
-          <Topic icon="skills" text="Skills" />
+          <Topic
+            icon="skills"
+            text="Skills"
+          />
 
-          <p class="mb-4">
-            PHP - Laravel: Familiarity with OOP design principles strong
-            understands of Laravel environment as the MVC life cycle. Able to
-            Maintain coding standards and follow good practices. On my studies
-            projects, I have used TDD. @OOP @MVC @API @Laravel @Composer
-            @Eloquent @Query Builder @TDD
-          </p>
-
-          <p class="mb-4">
-            JavaScript, HTML, and CSS @I've been in love with JS since day one,
-            even using just Vanilla JS and jQuery in my current job I keep
-            learning new technologies like Vue.js, Vuex, NPM, Webpack and
-            TailwindCSS. @ES6 @Vue.js @Webpack @jQuery @Vue.Js @NPM @Tailwind
-            CSS @Bootstrap
-          </p>
-
-          <p class="mb-4">
-            @MySQL @Even in my no-Development jobs, I was able to discuss
-            relational integrity with developers helping them to analyse the
-            data. @MySQL @MongoDB @MySQLWorkbench @Webpack @jQuery @Others
-          </p>
-
-          <p class="mb-4">
-            @Great knowledge using Git, git-flow a UNIX Terminal. Even using
-            MacOS all the time I'm familiar with LINUX commands as basic DevOps
-            configuration. @GIT @Unix Shell @MacOS @Linux @Visual Studio Code
-            @PHPStorm @Docker
-          </p>
+          <template v-for="skill in content.skills">
+            <p
+              :key="skill.name"
+              class="mb-4"
+            >
+              <b class="text-base">{{ skill.name }}</b>: {{ skill.description }}
+            </p>
+          </template>
         </div>
 
         <div class="mb-4">
-          <Topic icon="education" text="Education" />
-          <p class="font-bold whitespace-nowrap">
+          <Topic
+            icon="education"
+            text="Education"
+          />
+          <p class="font-bold text-sm whitespace-nowrap">
             Bachelor of Business Administration
           </p>
           <p>
@@ -61,32 +50,9 @@
             Santa Catarina (UNISUL)
           </p>
           <p>
-            This degree gave me technical skills and knowledge such as project
-            management, resource management, communication, team-working and
-            problem-solving.
+            {{ content.education.degree_description }}
           </p>
         </div>
-
-        <!-- Fabricio Zeferino
-
-EXPERIENCE
-eGov Solutions (epep.tv)
-Oct 2018 | Full-Stack PHP Developer
-
-Under the guidance of senior developer, I am responsible for analysis issues, design and code new features and improvements for an existing web application. Qualified to the development from the frontend end to the backend end throughout the Database. Worked in a Multitenancy platform for 35 councils.
-
-Refactored the core of the application reducing class with thousands of lines and hundreds of requests to just a few, applying methods as DRY and OOP and clean code, making it much easier to maintain and productive to escalate. Restructured the database, creating referential integrity between registers running multiples SYNC to fix old hardcoding.
-
-I have worked directly in a creation of a Microservice for reports, wherein a night routine all the data are migrated from MYSQL to a MongoDB making the reports much faster and the MYSQL free for others requests during the day. I helped to Migrate the server from Windows Server to Linux in AWS. Most relevant tools used in this role: Docker, Continues Integration, GitLab, SSH.
-
-EDUCATION
-Bachelor in Business Administration
-2006 – 2010 | University of South of Santa Catarina
-
-
-Key skills
-/*Skills*/
--->
       </div>
       <div class="sm:w-8/12 px-6 py-12 text-gray-900">
         <div class="mb-8">
@@ -103,16 +69,7 @@ Key skills
           </h1>
 
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam odio
-            tellus, interdum et cursus a, euismod eu dolor. Phasellus in neque
-            quam. Pellentesque consectetur suscipit feugiat. Nunc condimentum,
-            eros vitae cursus feugiat, lectus massa suscipit tortor, et laoreet
-            erat turpis et nibh. Sed viverra ipsum quis nulla consequat euismod.
-            Phasellus et congue leo. Sed egestas augue et gravida venenatis.
-            Nulla a commodo urna, eu porttitor nibh. In a tortor ut nisl feugiat
-            iaculis vestibulum vel nisl. Ut at fringilla nisl. Ut turpis ante,
-            euismod non nisi ac, lacinia tristique felis. Vivamus molestie
-            malesuada lectus in tempor.
+            {{ content.professional_summary }}
           </p>
         </div>
 
@@ -129,26 +86,42 @@ Key skills
             Work History
           </h1>
 
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam odio
-            tellus, interdum et cursus a, euismod eu dolor. Phasellus in neque
-            quam. Pellentesque consectetur suscipit feugiat. Nunc condimentum,
-            eros vitae cursus feugiat, lectus massa suscipit tortor, et laoreet
-            erat turpis et nibh. Sed viverra ipsum quis nulla consequat euismod.
-            Phasellus et congue leo. Sed egestas augue et gravida venenatis.
-            Nulla a commodo urna, eu porttitor nibh. In a tortor ut nisl feugiat
-            iaculis vestibulum vel nisl. Ut at fringilla nisl. Ut turpis ante,
-            euismod non nisi ac, lacinia tristique felis. Vivamus molestie
-            malesuada lectus in tempor.
-          </p>
+          <div
+            v-for="workHistory in content.work_histories"
+            :key="workHistory"
+          >
+            <p class="text-base font-bold mb-0">
+              {{ workHistory.company_name }}
+            </p>
+            <p class="font-medium text-sm mb-1">
+              {{ workHistory.job_title }} - {{ workHistory.start_at }} | {{ workHistory.end_at }}
+            </p>
+
+            <p
+              v-for="description in workHistory.description"
+              :key="description"
+              class="mb-1"
+            >
+              {{ description }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
+
 <script>
-export default {};
+import data from '@/pages/data.json'
+
+export default {
+  data(){
+    return {
+      content: data,
+    }
+  },
+}
 </script>
 
 <style>
@@ -159,8 +132,6 @@ export default {};
 
 body,
 html {
-  padding: 0;
-  margin: 0;
   width: 100%;
   min-height: 100vh;
   background-color: rgba(249, 250, 251);
@@ -173,11 +144,9 @@ html {
 
 .page {
   max-width: 8.5in;
-  /* max-height: 12in; */
-  /* box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; */
-  box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset,
-    rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+  box-shadow: rgba(255, 255, 255, 0.1) 0 1px 1px 0 inset,
+    rgba(50, 50, 93, 0.25) 0 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0 30px 60px -30px;
 }
 
 @media print {
@@ -187,7 +156,7 @@ html {
   h4,
   h5,
   h6 {
-    break-before: always;
+    /*break-before: always;*/
   }
 
   table,
@@ -201,7 +170,7 @@ html {
     height: 12in;
     background-color: #fff;
 
-    box-shadow: node;
+    /*box-shadow: initial;*/
   }
 
   @page {
